20e82d4b1ee25511d3bc0804042d947680b59b37
Zalika Ockleberry <Ockleberryzalika@gmail.com>
2016-09-14 09:53:12 -0400

    Fully FUNctional, with a working method

--- src/FactorialCalc.java
+++ src/FactorialCalc.java
@@ -3,46 +3,56 @@ import java.util.Scanner;
 public class FactorialCalc {
 
 	public static void main(String[] args) {
-		
+
 		//Welcome the user
 		System.out.println("Welcome to the Factorial Calculator! Factorials are fun and informative!");
-		
+
 		//Space
 		System.out.println();
-		
-		
+
+
 		int i ;
-		long factor = 1;
 		String cont = "y";
-		
-		
+
+		//Create Scanner
+		Scanner s = new Scanner(System.in);
+
 		//While loop
 		while (cont.equalsIgnoreCase("Y")) {
-		
+
 			//User Prompt
 			System.out.println("Enter an integer that's greater than 0 but less than 10: ");
-			//Create Scanner
-			Scanner s = new Scanner(System.in);
-			int num = s.nextInt();
-		
-		
-		//for loop
-		for (i = 1;i <= num; i++) 
-			factor = factor *i;
-			System.out.println(factor);
-		
-		//Continue
-		System.out.println("Continue? (y/n)");
-		s.nextLine();
-		cont =s.nextLine();
-		
+			long num = s.nextInt();
+
+			//Make sure you reset the variable by keeping this in the loop
+			long factor = 1;
+
+
+			//for loop 
+			for (i = 1;i <= num; i++)
+
+
+				//factor = factor * i
+				factor *=i;
+				System.out.println(factor);
+
+
+			//Continue
+			System.out.println("Continue? (y/n)");
+			s.nextLine();
+			cont =s.next();
+
+			//The ending thank you message
+			System.out.println("Thanks for using the calculator!");
 		}
 	}
-		} 
+} 
 
-	//Factorial formula
-		/*	public static long factorial(int n){ 
-			    if (n == 1) return 1; 
+//Factorial formula
+/*	public static int factorial(int n){ 
+			    if (n == 1){
+			     return 1;
+			     }else { 
 			    return n * factorial(n-1); 
 	}*/
 
